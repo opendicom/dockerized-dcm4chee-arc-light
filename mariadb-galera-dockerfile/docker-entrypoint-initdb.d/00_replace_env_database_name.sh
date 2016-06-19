4ced091984513b3598a0305258b15ddb4fc38ddd
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2016-06-19 00:58:13 -0300

    base image for mariadb+galera
    replication disabled by default

--- mariadb-galera-dockerfile/docker-entrypoint-initdb.d/00_replace_env_database_name.sh
+++ mariadb-galera-dockerfile/docker-entrypoint-initdb.d/00_replace_env_database_name.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Replace database name placeholder with the name defined in env variable (MYSQL_DATABASE).
 # This allows to define a custom name for the PACS database when starting a container.
 # The script is executed from the "docker-entrypoint.sh" script, and because
 # it starts with "00" it goes before the *.sql files defining the schema.
