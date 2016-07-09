b1bcd7ff0f13ccc8dfdc661ed1252ae992bf6ba2
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2016-07-09 16:25:58 -0300

    mariadb: dont't fail if replication is disabled and no user is given

--- mariadb-dockerfile/docker-entrypoint-initdb.d/00_init.sh
+++ mariadb-dockerfile/docker-entrypoint-initdb.d/00_init.sh
@@ -3,24 +3,10 @@
 # Execute custom actions on startup. The script name starts with "00",
 # so it gets executed before the *.sql files defining the schema.
 
-# Create .sql file containing replication user
-replication_file='/docker-entrypoint-initdb.d/30_replication_user.sql'
-echo "GRANT USAGE ON *.* to '$REPLICATION_USER'@'%' IDENTIFIED BY '$REPLICATION_PASSWORD';" >> $replication_file;
-echo "GRANT ALL PRIVILEGES on *.* to '$REPLICATION_USER'@'%';" >> $replication_file
-echo "FLUSH PRIVILEGES;" >> $replication_file
-
-# Allow to define a custom name for the PACS database when starting a
-# container using MYSQL_DATABASE environmental variable
-cd /docker-entrypoint-initdb.d/ && for file in *.sql; do
-    echo "use $MYSQL_DATABASE;" > tmp.sql
-    cat $file >> tmp.sql
-    rm $file
-    mv tmp.sql $file
-done
-
 # Use environmental variables to configure replication
-config_file='/etc/mysql/conf.d/server.cnf'
 if [ -n "$WSREP_ON" ]; then
+    # Create configuration file
+    config_file='/etc/mysql/conf.d/server.cnf'
     echo "[galera]" >> $config_file
     echo "wsrep_on = ON" >> $config_file
     echo "wsrep_debug = ON" >> $config_file
@@ -31,4 +17,19 @@ if [ -n "$WSREP_ON" ]; then
     echo "wsrep_sst_auth = "$REPLICATION_USER":"$REPLICATION_PASSWORD >> $config_file
     echo "wsrep_node_address = "$WSREP_NODE_ADDRESS >> $config_file
     echo "wsrep_node_name = "$WSREP_NODE_NAME >> $config_file
+
+    # Create .sql file containing replication user
+    replication_file='/docker-entrypoint-initdb.d/30_replication_user.sql'
+    echo "GRANT USAGE ON *.* to '$REPLICATION_USER'@'%' IDENTIFIED BY '$REPLICATION_PASSWORD';" >> $replication_file;
+    echo "GRANT ALL PRIVILEGES on *.* to '$REPLICATION_USER'@'%';" >> $replication_file
+    echo "FLUSH PRIVILEGES;" >> $replication_file
 fi
+
+# Allow to define a custom name for the PACS database when starting a
+# container using MYSQL_DATABASE environmental variable
+cd /docker-entrypoint-initdb.d/ && for file in *.sql; do
+    echo "use $MYSQL_DATABASE;" > tmp.sql
+    cat $file >> tmp.sql
+    rm $file
+    mv tmp.sql $file
+done
